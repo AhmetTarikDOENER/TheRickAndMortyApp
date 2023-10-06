4abc33599dbf7da610cb8d137d9f86cbd8fcc424
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-10-06 17:38:27 +0300

    Added search request builder

--- TheRickAndMortyApp/ViewModels/Search/RMSearchViewViewModel.swift
+++ TheRickAndMortyApp/ViewModels/Search/RMSearchViewViewModel.swift
@@ -13,9 +13,11 @@ final class RMSearchViewViewModel {
     
     private var optionMap: [RMSearchInputViewViewModel.DynamicOption: String] = [:]
     
+    private var searchText = ""
+    
     private var optionMapUpdateBlock: (((RMSearchInputViewViewModel.DynamicOption, String)) -> Void)?
     
-    private var searchText = ""
+    private var searchResultHandler: (() -> Void)?
     
     //MARK: - Init
     
@@ -25,8 +27,36 @@ final class RMSearchViewViewModel {
     
     //MARK: - Public
     
+    public func registerSearchResultHandler(_ block: @escaping () -> Void) {
+        self.searchResultHandler = block
+    }
+    
     public func executeSearch() {
-        
+        // For testing
+        searchText = "Rick"
+        // Build arguments
+        var queryParams: [URLQueryItem] = [
+            URLQueryItem(name: "name", value: searchText)
+        ]
+        // Add Options
+        queryParams.append(contentsOf: optionMap.enumerated().compactMap({
+            _, element in
+            let key: RMSearchInputViewViewModel.DynamicOption = element.key
+            let value: String =  element.value
+            return URLQueryItem(name: key.queryArgument, value: value)
+        }))
+        // Create request
+        let request = RMRequest(endpoint: config.type.endpoint, queryParameters: queryParams)
+        print(request.url?.absoluteString)
+        RMService.shared.execute(request, expecting: RMGetAllCharactersResponse.self) {
+            result in
+            switch result {
+            case .success(let model):
+                print("Search results found \(model.results.count)")
+            case .failure:
+                break
+            }
+        }
     }
     
     public func set(query text: String) {

4124e92108003acb1f6c2f92c2851e1f52e318ee
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-10-09 23:19:27 +0300

    Search results collection view pagination

--- TheRickAndMortyApp/Views/Search/RMSearchResultsView.swift
+++ TheRickAndMortyApp/Views/Search/RMSearchResultsView.swift
@@ -46,7 +46,7 @@ final class RMSearchResultsView: UIView {
         collectionView.register(RMCharacterEpisodeCollectionViewCell.self,
                                 forCellWithReuseIdentifier: RMCharacterEpisodeCollectionViewCell.cellIdentifier
         )
-        
+        // Footer for loading
         collectionView.register(RMFooterLoadingCollectionReusableView.self,
                                 forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
                                 withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier
@@ -194,6 +194,31 @@ extension RMSearchResultsView: UICollectionViewDelegate, UICollectionViewDataSou
         let width = bounds.width - 20
         return CGSize(width: width, height: 100)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionFooter,
+              let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                           withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier,
+                                                                           for: indexPath
+              ) as? RMFooterLoadingCollectionReusableView else {
+            fatalError("Unsupported")
+        }
+        
+        if let viewModel = viewModel, viewModel.shouldShowLoadMoreIndicator {
+            footer.startAnimating()
+        }
+        
+        return footer
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        guard let viewModel = viewModel,
+            viewModel.shouldShowLoadMoreIndicator else {
+            return .zero
+        }
+        return CGSize(width: collectionView.frame.width, height: 100)
+    }
 }
 
 //MARK: - ScrollViewDelegate
@@ -210,7 +235,31 @@ extension RMSearchResultsView: UIScrollViewDelegate {
     }
     
     private func handleCharacterOrEpisodePagination(scrollView: UIScrollView) {
+        guard let viewModel = viewModel,
+              !collectionViewCellViewModels.isEmpty,
+              viewModel.shouldShowLoadMoreIndicator,
+              !viewModel.isLoadingMoreResults else {
+            return
+        }
         
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) {
+            [weak self] t in
+            let offset = scrollView.contentOffset.y
+            let totalContentHeight = scrollView.contentSize.height
+            let totalScrollViewFixedHeight = scrollView.frame.size.height
+            
+            if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120) {
+                viewModel.fetchAdditionalResults{
+                    //Refresh table
+                    [weak self] newResult in
+                    self?.tableView.tableFooterView = nil
+                    self?.collectionViewCellViewModels = newResult
+                    
+                    print("Should add more results cells for search result: \(newResult.count)")
+                }
+            }
+            t.invalidate()
+        }
     }
     
     private func handleLocationPagination(scrollView: UIScrollView) {
@@ -229,7 +278,7 @@ extension RMSearchResultsView: UIScrollViewDelegate {
             
             if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120) {
                 DispatchQueue.main.async {
-                    self?.showLoadingIndicator()
+                    self?.showTableLoadingIndicator()
                 }
                 viewModel.fetchAdditionalLocations {
                     //Refresh table
@@ -243,7 +292,7 @@ extension RMSearchResultsView: UIScrollViewDelegate {
         }
     }
     
-    private func showLoadingIndicator() {
+    private func showTableLoadingIndicator() {
         let footer = RMTableLoadingFooterView(
             frame: CGRect(x: 0, y: 0, width: frame.size.width, height: 100)
         )
